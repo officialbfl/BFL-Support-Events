9407b9643213d1c8d7e18762507d630c1222a988
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-04-17 21:11:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -358,7 +358,7 @@ async def on_message(message):
 			
 			embed = discord.Embed(title='', color=0x3391D0)
 			embed.add_field(name="__Announcement__", value= "```%s```" % (" ".join(args[1:])), inline=False)
-			embed.add_field(name="By:", value="{0.author}".format(message), inline=False)
+			embed.add_field(name="By", value="{0.author}".format(message), inline=False)
 			embed.set_thumbnail(url="https://media.discordapp.net/attachments/566631626545823786/568131998523064320/ef94710266f57ca3e88c8cbe05fc4a8a.png?width=192&height=192")
 			a=await client.send_message(channel, embed=embed)
 			await client.add_reaction(a, "👍")
@@ -374,7 +374,8 @@ async def on_message(message):
 			await client.send_message(channel, "@everyone")
 			embed = discord.Embed(title='', color=0x3391D0)
 			embed.add_field(name="__Announcement__", value= "```%s```" % (" ".join(args[1:])), inline=False)
-			embed.add_field(name="By:", value="{0.message.author}".format(message), inline=False)
+			embed.add_field(name="By", value="{0.author}".format(message), inline=False)
+			embed.add_field(name="Server", value="{0.server.name}".format(message), inline=True)
 			embed.set_thumbnail(url="https://media.discordapp.net/attachments/566631626545823786/568131998523064320/ef94710266f57ca3e88c8cbe05fc4a8a.png?width=192&height=192")
 			a=await client.send_message(channel, embed=embed)
 			await client.add_reaction(a, "👍")
