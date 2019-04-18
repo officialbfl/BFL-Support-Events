70d1f917b1250370f79c6e991940ab0e1464f463
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-04-18 21:22:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -351,7 +351,8 @@ async def on_message(message):
 	if message.content.startswith('-announce'):
 		if message.content[9:] =="":
 			await client.send_message(message.channel, 'Error. Type in an announcement.')
-		else:
+	else:
+		if message.author.id == "519231052556795944" or message.author.id == "562000458181246982":
 			await client.send_message(message.channel, '{0.author.mention} Your announcement has been sent succesfully!'.format(message))
 			args = message.content.split(" ")		
 			channel=client.get_channel('568135270210207755')
